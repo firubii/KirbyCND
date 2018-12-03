d349b9f0d6ec24d7bfd4a95cd42193e15b054b5b
FruitMage <fruitymage@gmail.com>
2018-12-02 18:47:07 -0600

    Add project files.

---
+++ KirbyCND/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace KirbyCND
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.entryList = new System.Windows.Forms.TreeView();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(432, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Enabled = false;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.entryList);
+            this.groupBox1.Location = new System.Drawing.Point(13, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(410, 410);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Entries";
+            // 
+            // entryList
+            // 
+            this.entryList.Location = new System.Drawing.Point(7, 20);
+            this.entryList.Name = "entryList";
+            this.entryList.Size = new System.Drawing.Size(397, 384);
+            this.entryList.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KirbyCND";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TreeView entryList;
+    }
+}
+
---
+++ KirbyCND/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace KirbyCND
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ReadCND(BinaryReader reader)
+        {
+            reader.BaseStream.Seek(0x20, SeekOrigin.Begin);
+            reader.BaseStream.Seek(reader.ReadUInt32(), SeekOrigin.Begin);
+            uint modelDataSize = reader.ReadUInt32();
+            List<uint> modelDataOffsetList = new List<uint>();
+            entryList.Nodes.Add("Visual Data");
+            for (int i = 0; i < modelDataSize; i++)
+            {
+                modelDataOffsetList.Add(reader.ReadUInt32());
+            }
+            for (int i = 0; i < modelDataSize; i++)
+            {
+                reader.BaseStream.Seek(modelDataOffsetList[i], SeekOrigin.Begin);
+                uint valOffset = reader.ReadUInt32();
+                uint varOffset = reader.ReadUInt32();
+                reader.BaseStream.Seek(valOffset, SeekOrigin.Begin);
+                uint valLength = reader.ReadUInt32();
+                string val = string.Join("", reader.ReadChars((int)valLength));
+                reader.BaseStream.Seek(varOffset, SeekOrigin.Begin);
+                uint varLength = reader.ReadUInt32();
+                string var = string.Join("", reader.ReadChars((int)varLength));
+                entryList.Nodes[0].Nodes.Add($"{var} {val}");
+                reader.BaseStream.Seek(modelDataOffsetList[i] + 0xC, SeekOrigin.Begin);
+                reader.BaseStream.Seek(reader.ReadUInt32(), SeekOrigin.Begin);
+                uint variableListCount = reader.ReadUInt32();
+                List<uint> variableOffsets = new List<uint>();
+                for (int v = 0; v < variableListCount; v++)
+                {
+                    variableOffsets.Add(reader.ReadUInt32());
+                }
+                for (int v = 0; v < variableListCount; v++)
+                {
+                    reader.BaseStream.Seek(variableOffsets[v], SeekOrigin.Begin);
+                    uint nameOffset = reader.ReadUInt32();
+                    uint typeOffset = reader.ReadUInt32();
+                    reader.BaseStream.Seek(nameOffset, SeekOrigin.Begin);
+                    uint nameLength = reader.ReadUInt32();
+                    string name = string.Join("", reader.ReadChars((int)nameLength));
+                    reader.BaseStream.Seek(typeOffset, SeekOrigin.Begin);
+                    uint typeLength = reader.ReadUInt32();
+                    string type = string.Join("", reader.ReadChars((int)typeLength));
+                    entryList.Nodes[0].Nodes[i].Nodes.Add($"{type} {name}");
+                    reader.BaseStream.Seek(variableOffsets[v] + 0xC, SeekOrigin.Begin);
+                    uint variableCount = reader.ReadUInt32();
+                    if (type != "String")
+                    {
+                        for (int d = 0; d < variableCount; d++)
+                        {
+                            switch (type)
+                            {
+                                case "Int":
+                                    {
+                                        entryList.Nodes[0].Nodes[i].Nodes[v].Nodes.Add(reader.ReadUInt32().ToString());
+                                        break;
+                                    }
+                                case "Vec3":
+                                case "Float":
+                                    {
+                                        entryList.Nodes[0].Nodes[i].Nodes[v].Nodes.Add(reader.ReadSingle().ToString());
+                                        break;
+                                    }
+                                case "Bool":
+                                    {
+                                        entryList.Nodes[0].Nodes[i].Nodes[v].Nodes.Add(reader.ReadBoolean().ToString());
+                                        v = (int)variableCount;
+                                        break;
+                                    }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        uint stringLen = reader.ReadUInt32();
+                        entryList.Nodes[0].Nodes[i].Nodes[v].Nodes.Add(Encoding.UTF8.GetString(reader.ReadBytes((int)stringLen)));
+                    }
+                }
+            }
+            reader.BaseStream.Seek(0x30, SeekOrigin.Begin);
+            reader.BaseStream.Seek(reader.ReadUInt32(), SeekOrigin.Begin);
+            uint size = reader.ReadUInt32();
+            List<uint> offsetList = new List<uint>();
+            entryList.Nodes.Add("Render Data");
+            for (int i = 0; i < size; i++)
+            {
+                offsetList.Add(reader.ReadUInt32());
+            }
+            for (int i = 0; i < size; i++)
+            {
+                reader.BaseStream.Seek(offsetList[i], SeekOrigin.Begin);
+                uint valOffset = reader.ReadUInt32();
+                uint varOffset = reader.ReadUInt32();
+                reader.BaseStream.Seek(valOffset, SeekOrigin.Begin);
+                uint valLength = reader.ReadUInt32();
+                string val = Encoding.UTF8.GetString(reader.ReadBytes((int)valLength));
+                reader.BaseStream.Seek(varOffset, SeekOrigin.Begin);
+                uint varLength = reader.ReadUInt32();
+                string var = Encoding.UTF8.GetString(reader.ReadBytes((int)varLength));
+                entryList.Nodes[1].Nodes.Add($"{var}: {val}");
+                reader.BaseStream.Seek(offsetList[i] + 0xC, SeekOrigin.Begin);
+                reader.BaseStream.Seek(reader.ReadUInt32(), SeekOrigin.Begin);
+                uint variableListCount = reader.ReadUInt32();
+                List<uint> variableOffsets = new List<uint>();
+                for (int f = 0; f < variableListCount; f++)
+                {
+                    variableOffsets.Add(reader.ReadUInt32());
+                }
+                for (int f = 0; f < variableListCount; f++)
+                {
+                    reader.BaseStream.Seek(variableOffsets[f], SeekOrigin.Begin);
+                    uint variableOffset = reader.ReadUInt32();
+                    uint typeOffset = reader.ReadUInt32();
+                    reader.BaseStream.Seek(variableOffset, SeekOrigin.Begin);
+                    uint variableLength = reader.ReadUInt32();
+                    string variable = Encoding.UTF8.GetString(reader.ReadBytes((int)variableLength));
+                    reader.BaseStream.Seek(typeOffset, SeekOrigin.Begin);
+                    uint typeLength = reader.ReadUInt32();
+                    string type = Encoding.UTF8.GetString(reader.ReadBytes((int)typeLength));
+                    entryList.Nodes[1].Nodes[i].Nodes.Add($"{type} {variable}");
+                    reader.BaseStream.Seek(variableOffsets[f] + 0xC, SeekOrigin.Begin);
+                    uint variableCount = reader.ReadUInt32();
+                    if (type != "String")
+                    {
+                        for (int v = 0; v < variableCount; v++)
+                        {
+                            switch (type)
+                            {
+                                case "Int":
+                                    {
+                                        entryList.Nodes[1].Nodes[i].Nodes[f].Nodes.Add(reader.ReadUInt32().ToString());
+                                        break;
+                                    }
+                                case "Vec3":
+                                case "Float":
+                                    {
+                                        entryList.Nodes[1].Nodes[i].Nodes[f].Nodes.Add(reader.ReadSingle().ToString());
+                                        break;
+                                    }
+                                case "Bool":
+                                    {
+                                        entryList.Nodes[1].Nodes[i].Nodes[f].Nodes.Add(reader.ReadBoolean().ToString());
+                                        v = (int)variableCount;
+                                        break;
+                                    }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        uint stringLen = reader.ReadUInt32();
+                        entryList.Nodes[1].Nodes[i].Nodes[f].Nodes.Add(Encoding.UTF8.GetString(reader.ReadBytes((int)stringLen)));
+                    }
+                }
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "CNDBIN Files|*.cndbin";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(open.FileName))
+                {
+                    entryList.Nodes.Clear();
+                    entryList.BeginUpdate();
+                    BinaryReader reader = new BinaryReader(new FileStream(open.FileName, FileMode.Open));
+                    ReadCND(reader);
+                    entryList.EndUpdate();
+                }
+            }
+        }
+    }
+}
